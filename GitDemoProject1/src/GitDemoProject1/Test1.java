324f1f231b61185f591c0ff876db6321da334663
JoseWongg <j.l.wong@edu.salford.ac.uk>
2023-02-09 17:48:24 +0000

    Initial commit

---
+++ GitDemoProject1/src/GitDemoProject1/Test1.java
@@ -0,0 +1,19 @@
+package GitDemoProject1;
+
+
+class Test1 
+{
+
+	public static void main(String[]args) 
+	{
+		String hungry = "yes";
+		
+		if(hungry.equals("yes"))
+		{
+			System.out.println("eat something...");
+		}else 
+			{
+				System.out.println("Dont eat");
+			}
+	}
+}
